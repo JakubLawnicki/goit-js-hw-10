c99b1143c1f1da4729ac15998e8e47dc5ef034d7
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2023-09-05 00:07:43 +0200

    e

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,41 @@
+import axios from 'axios';
+
 export function fetchBreeds() {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     fetch(
       'https://api.thecatapi.com/v1/breeds?api_key=live_y4UBJpWFDyRXMCTGfGBilRBknPor8oQfujHTprh9Wc5GLEprvfb2C3TWjhs6htue'
     )
       .then(response => {
         return response.json();
       })
-      .then(data => resolve(data))
-      .catch(error => error('Request failed'));
+      .then(data => resolve(data));
+    //   .catch(error => error('Request failed'));
+    //   axios
+    //     .get(
+    //       'https://api.thecatapi.com/v1/breeds?api_key=live_y4UBJpWFDyRXMCTGfGBilRBknPor8oQfujHTprh9Wc5GLEprvfb2C3TWjhs6htue'
+    //     )
+    //     .then(result => result.data)
+    //     .catch(error => error('Request failed'));
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return new Promise(resolve => {
+    fetch(
+      `https://api.thecatapi.com/v1/images/search?breeds_ids=${breedId}&api_key=live_y4UBJpWFDyRXMCTGfGBilRBknPor8oQfujHTprh9Wc5GLEprvfb2C3TWjhs6htue`
+    ).then(response => {
+      fetchBreeds().then(catsList =>
+        catsList.forEach(cat => {
+          if (cat.id === `${breedId}`) {
+            const catsData = {
+              name: `${cat.name}`,
+              descr: `${cat.description}`,
+              temp: `${cat.temperament}`,
+            };
+            console.log(catsData);
+          }
+        })
+      );
+    });
   });
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import { fetchBreeds } from './cat-api.js';
+import { fetchCatByBreed } from './cat-api.js';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_y4UBJpWFDyRXMCTGfGBilRBknPor8oQfujHTprh9Wc5GLEprvfb2C3TWjhs6htue';
 
+const axios = require('axios');
 const select = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 let options = [];
+let catInfoArray = [];
 
 window.addEventListener('load', fetchBreeds);
 
@@ -18,3 +22,19 @@ fetchBreeds().then(catsList => {
   });
   select.append(...options);
 });
+
+select.addEventListener('change', e => {
+  fetchCatByBreed(e.currentTarget.value);
+});
+
+// fetchCatByBreed().then(catItem => {
+//   console.log(catItem);
+//   catInfoArray.push(document.createElement('img'));
+//   catInfoArray.push(document.createElement('h1'));
+//   catInfoArray.push(document.createElement('p'));
+//   catInfoArray.push(document.createElement('span'));
+//   catInfo.append(...catInfoArray);
+//   const catImage = document.querySelector('img');
+//   //   catImage.setAttribute('src', );
+//   console.log(catImage);
+// });
